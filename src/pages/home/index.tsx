161fae8c94262d5a29acf2e58038ff86b4a78f36
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-08-09 21:25:42 +0530

    initial commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/common/navbar"
+import Footer from "../../components/common/footer"
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
